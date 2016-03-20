18c993dffd14e629561879f61d098b84f49d4075
Jim van Vliet <jimvanvliet98@gmail.com>
2016-03-20 18:34:42 +0100

    Initial commit

---
+++ app.ts
@@ -0,0 +1,27 @@
+/// <reference path="lib/phaser.d.ts"/>
+
+class SimpleGame {
+
+    constructor() {
+        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
+    }
+
+    game: Phaser.Game;
+
+
+    preload() {
+        this.game.load.image('logo', 'images/backgrounds/003-StarlitSky01.jpg');
+    }
+
+    create() {
+        var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
+        logo.anchor.setTo(0.5, 0.5);
+    }
+
+}
+
+window.onload = () => {
+
+    var game = new SimpleGame();
+
+};
